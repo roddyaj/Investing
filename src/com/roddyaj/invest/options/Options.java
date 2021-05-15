7ea82928d0890210b5736131652eec3a8ed8fea5
Adam Roddy <aroddy@mail.com>
2021-05-15 13:37:54 -0600

    More refactoring in Options

--- src/com/roddyaj/invest/options/Options.java
+++ src/com/roddyaj/invest/options/Options.java
@@ -15,6 +15,11 @@ import com.roddyaj.invest.util.FileUtils;
 
 public class Options implements Program
 {
+	public static void main(String[] args)
+	{
+		new Options().run(null);
+	}
+
 	@Override
 	public void run(String[] args)
 	{
@@ -22,26 +27,45 @@ public class Options implements Program
 		records = records.stream().filter(r -> r.size() > 1 && (r.get(1).startsWith("Sell to") || r.get(1).startsWith("Buy to")))
 				.collect(Collectors.toList());
 		writeCsv(records);
+//		records.forEach(System.out::println);
 	}
 
 	private void writeCsv(Collection<? extends CSVRecord> records)
 	{
 		List<String> linesOut = new ArrayList<>();
 		linesOut.add("Buy Date,Option,Days,Strike,Price,Premium,Annualized Return");
-		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-		for (CSVRecord record : records)
+		for (CSVRecord csvRecord : records)
 		{
-			LocalDate buyDate = LocalDate.parse(record.get(0), formatter);
-			String option = record.get(2);
-			double price = Double.parseDouble(record.get(5).replace("$", ""));
-			double premium = Double.parseDouble(record.get(7).replace("$", ""));
-			String[] tokens = option.split(" ");
-			LocalDate expiryDate = LocalDate.parse(tokens[1], formatter);
-			double strike = Double.parseDouble(tokens[2]);
-			long days = ChronoUnit.DAYS.between(buyDate, expiryDate);
-			double annualReturn = premium / strike * (365.0 / days);
-			linesOut.add(String.format("%s,%s,%d,%.2f,%.2f,%.2f,%.1f", buyDate, option, days, strike, price, premium, annualReturn));
+			Record record = new Record(csvRecord);
+			double annualReturn = record.premium / record.strike * (365.0 / record.days);
+			linesOut.add(String.format("%s,%s,%d,%.2f,%.2f,%.2f,%.1f", record.buyDate, record.option, record.days, record.strike, record.price,
+					record.premium, annualReturn));
 		}
 		FileUtils.writeLines("options_history.csv", linesOut);
+	}
+
+	private static class Record
+	{
+		private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+		public final LocalDate buyDate;
+		public final String option;
+		public final double price;
+		public final double premium;
+		public final LocalDate expiryDate;
+		public final double strike;
+		public final long days;
+
+		public Record(CSVRecord record)
+		{
+			buyDate = LocalDate.parse(record.get(0), FORMATTER);
+			option = record.get(2);
+			price = Double.parseDouble(record.get(5).replace("$", ""));
+			premium = Double.parseDouble(record.get(7).replace("$", ""));
+			String[] tokens = option.split(" ");
+			expiryDate = LocalDate.parse(tokens[1], FORMATTER);
+			strike = Double.parseDouble(tokens[2]);
+			days = ChronoUnit.DAYS.between(buyDate, expiryDate);
+		}
 	}
 }

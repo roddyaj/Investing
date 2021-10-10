f4b70547616e4c1918881145fa99d3d0b9ac9afe
Adam Roddy <aroddy@mail.com>
2021-10-10 12:09:34 -0600

    Update build and run scripts

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-## Clean
-#[ -d target ] && rm -r target
-#
-#set -e
-#
-## Build
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.12.5/jackson-annotations-2.12.5.jar;"
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-core/2.12.5/jackson-core-2.12.5.jar;"
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.12.5/jackson-databind-2.12.5.jar;"
-#classpath+="../../.m2/repository/org/apache/commons/commons-csv/1.9.0/commons-csv-1.9.0.jar;"
-#classpath+="../../.m2/repository/com/squareup/okhttp3/okhttp/4.9.1/okhttp-4.9.1.jar;"
-#classpath+="../../.m2/repository/com/squareup/okio/okio/2.8.0/okio-2.8.0.jar;"
-#classpath+="../../.m2/repository/org/jetbrains/kotlin/kotlin-stdlib/1.4.10/kotlin-stdlib-1.4.10.jar;"
-#javac -cp $classpath -sourcepath src/main/java src/main/java/com/roddyaj/invest/Main.java -d target/classes
+INSTALL=install
+
+set -x
 
 mvn clean
-mvn compile
+rm -r $INSTALL
+mvn package
+mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=$INSTALL
+cp target/*.jar $INSTALL
--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.12.5/jackson-annotations-2.12.5.jar;"
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-core/2.12.5/jackson-core-2.12.5.jar;"
-#classpath+="../../.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.12.5/jackson-databind-2.12.5.jar;"
-#classpath+="../../.m2/repository/org/apache/commons/commons-csv/1.9.0/commons-csv-1.9.0.jar;"
-#classpath+="../../.m2/repository/com/squareup/okhttp3/okhttp/4.9.1/okhttp-4.9.1.jar;"
-#classpath+="../../.m2/repository/com/squareup/okio/okio/2.8.0/okio-2.8.0.jar;"
-#classpath+="../../.m2/repository/org/jetbrains/kotlin/kotlin-stdlib/1.4.10/kotlin-stdlib-1.4.10.jar;"
-#classpath+='target/classes'
-#java -cp $classpath com.roddyaj.invest.Main $@
+INSTALL=install
 
-args=$@
-mvn exec:java -Dexec.args="${args}"
+java -cp $INSTALL/'*' com.roddyaj.invest.Main $@
